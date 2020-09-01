ad89a3e7b0c9043b709079b91b55bafd718bbea9
Sean Leif Hellier <sean@newtropolis.com>
2020-09-01 12:33:09 -0400

    fixed search bug

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
+
+// api.openweathermap.org/data/2.5/weather?q=`search1&appid=5a06a895e3eaf75b5ab8100f55b8cd46
 const API_KEY = "5a06a895e3eaf75b5ab8100f55b8cd46";
 
 export const fetchWeather = async (query) => {
