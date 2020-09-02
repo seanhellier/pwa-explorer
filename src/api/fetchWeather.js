3afc8060989f59c1fac7704ed1becf1fcaa1d874
Sean Leif Hellier <sean@newtropolis.com>
2020-09-01 20:07:20 -0400

    fixed apikey problem (thanks tony ho)

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const URL = "api.openweathermap.org/data/2.5/weather";
+const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 const API_KEY = "5a06a895e3eaf75b5ab8100f55b8cd46";
 
 export const fetchWeather = async (query) => {
-	const { data } = axios.get(URL, {
+	const { data } = await axios.get(URL, {
 		params: {
 			q: query,
 			units: "metric",
-			appid: "API_KEY",
+			appid: API_KEY,
 		},
 	});
 
